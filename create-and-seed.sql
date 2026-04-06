1e1b64e9b4b3eb97f106e8b4df0be05b62dc8703
Syed <xayanmay@gmail.com>
2026-04-05 23:22:06 -0400

    Created table, still working on it

--- create-and-seed.sql
+++ create-and-seed.sql
@@ -10,7 +10,7 @@
 --   C. A gym class schedule
 --      Suggested columns: class_name, instructor, day_of_week, capacity, enrolled, difficulty
 --
---   D. A movie collection
+--   D. A movie collection 
 --      Suggested columns: title, director, genre, year, runtime_minutes, rating
 --
 --   E. A bookstore inventory
@@ -33,14 +33,35 @@
 -- Step 1: Create the database
 -- ============================================================
 
-
+DROP DATABASE IF EXISTS music_db;
+CREATE DATABASE music_db;
 
 -- ============================================================
 -- Step 2: Create the table
 -- ============================================================
-
+CREATE TABLE songs (
+  song_id           SERIAL PRIMARY KEY,
+  title             TEXT NOT NULL,
+  artist            TEXT NOT NULL,
+  genre             TEXT NOT NULL,
+  release_year      INT NOT NULL,
+  duration_seconds  INT NOT NULL,
+  streams           INT DEFAULT 0;
+  UNIQUE (title, artist)
+);
 
 
 -- ============================================================
 -- Step 3: Insert seed data (at least 8 rows)
 -- ============================================================
+INSERT INTO songs (title, artist, genre, release_year, duration_seconds, streams) VALUES
+  ('Trust', 'Brent Faiyaz', 'R&B', 2018, 180, 50000000),
+  ('Clouded', 'Brent Faiyaz', 'R&B', 2020, 210, 70000000),
+  ('Dead Man Walking', 'Brent Faiyaz', 'R&B', 2021, 240, 65000000),
+  ('Dior', 'Pop Smoke', 'Drill', 2019, 210, 300000000),
+  ('Mood Swings', 'Pop Smoke', 'Drill', 2020, 215, 450000000),
+  ('What You Know Bout Love', 'Pop Smoke', 'Drill', 2020, 200, 350000000),
+  ('Blinding Lights', 'The Weeknd', 'R&B', 2019, 200, 1200000000),
+  ('Goosebumps', 'Travis Scott', 'Hip-Hop', 2016, 244, 850000000),
+  ('Snooze', 'SZA', 'R&B', 2023, 202, 600000000),
+  ('Lucid Dreams', 'Juice WRLD', 'Hip-Hop', 2018, 239, 950000000);
